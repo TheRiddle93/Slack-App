4ef1a1d833eb95209a108a05a2eaacf7fd7fdbe8
heio95 <davidkyvik@gmail.com>
2021-03-01 14:26:03 +0100

    adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// . means current directory, then / for other directories/folders inside the current directory, then / the file you wish to import.
+import Chat from "./components/Chat";
+import Login from "./components/Login";
+import styled from "styled-components";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Container>
+          <Header />
+          <Main>
+            <Sidebar />
+            <Switch>
+              <Route path="/room">
+                <Chat />
+              </Route>
+              <Route path="/">
+                <Login />
+              </Route>
+            </Switch>
+          </Main>
+        </Container>
+      </Router>
     </div>
   );
 }
 
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: grid;
+  grid-template-rows: 30px auto;
+`;
+
+const Main = styled.div`
+  background: white;
+  display: grid;
+  grid-template-columns: 260px auto;
+`;
+
 export default App;
+
+// index.js imports the code in app.js. and linkes it to html, by getElementById('root'). So everything that is made in react is linked to the index html through div id 'root'
